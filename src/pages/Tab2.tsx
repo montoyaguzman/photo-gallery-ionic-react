87f037a2326c2b1d8d598b289c9a6a21f5994bcb
Jose Montoya <montoyaguzman7@gmail.com>
2021-05-09 14:39:35 -0500

    FEAT : Funcion para la web y cambios visuales en Tab2

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+         IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+         IonCol, IonImg, IonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+
+const takePhoto = () => {
+  console.log('taking photo...')
+}
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+      <IonContent>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
